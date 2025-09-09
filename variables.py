f074b2a4825594024539fd83094f52b66574e87c
alidarrit <ai7790@g.rit.edu>
2025-09-09 09:52:12 +0400

    variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+def variable_practice():
+    age = 18 * 12
+    days_in_a_year = 365
+    pet_name = "Buddy"
+    pi = 3.1415
+
+    print("Age: ", age)
+    print("Days: ", days_in_a_year)
+    print("Pet's name: ", pet_name)
+    print("Pi: ", pi)
+variable_practice()
